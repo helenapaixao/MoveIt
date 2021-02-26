5a42ca542ad3881f0860e30889d7cc3db0df457a
Helena Paixão <hp.helenapaixao@gmail.com>
2021-02-25 21:22:10 -0400

    :fire: feat: config fonts in project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                </Head>
+            </Html>
+
+        )
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ import {ExperienceBar}  from '../components/Experience'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
-      </Head>
+     
 
     <ExperienceBar/>
        </div>
