37a977be1465b3103600106802e067fd1fbfcb4d
Helena Paixão <hp.helenapaixao@gmail.com>
2021-02-25 21:37:12 -0400

    :tada:feat:create profiles page

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/helenapaixao.png" alt="Helena Paixão" />
+      <div>
+        <strong>Helena Paixão</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
-import Head from 'next/head'
-import {ExperienceBar}  from '../components/Experience'
+import Head from "next/head";
+import { ExperienceBar } from "../components/Experience";
+import { Profile } from "../components/Profile";
 
 export default function Home() {
   return (
     <div className="container">
-     
+      <ExperienceBar />
 
-    <ExperienceBar/>
-       </div>
-    
-  )
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
 }
