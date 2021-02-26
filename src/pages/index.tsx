1a2a348e7b3e7470aa9f5cab83c2fdf70639ac0e
Helena Paixão <hp.helenapaixao@gmail.com>
2021-02-25 21:47:42 -0400

    :bug:fix:config profile styles link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { ExperienceBar } from "../components/Experience";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css'
+import styles from '../styles/components/Profile.module.css'
 
 export default function Home() {
   return (
